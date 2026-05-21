6ed43338e8a9d84b974692bc7f1ea00dfa2d881a
Javier272 <147571233+Javier272@users.noreply.github.com>
2026-05-21 08:16:26 -0600

    7

--- React/demo/src/Sidebar.jsx
+++ React/demo/src/Sidebar.jsx
@@ -33,7 +33,7 @@ function Sidebar({ setVista }) {
           onClick={() => setModo("team")}
         >
         <img src={team} alt="team" className="img-sideButton" />
-          Teamsssss
+          Teamssseeess
         </button>
 
         <hr className="sidebar-divider" />
