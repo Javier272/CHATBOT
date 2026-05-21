ee9007a309d84b97336a718c247103deb6624dbd
Javier272 <147571233+Javier272@users.noreply.github.com>
2026-05-21 07:30:10 -0600

    prueba-devops

--- React/demo/src/Sidebar.jsx
+++ React/demo/src/Sidebar.jsx
@@ -25,7 +25,7 @@ function Sidebar({ setVista }) {
         >
         <img src={user} alt="user" className="img-sideButton" />
 
-          Individual
+          Individualssssss
         </button>
 
         <button 
